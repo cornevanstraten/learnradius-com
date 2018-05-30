840b890af634116c7be675bfa2090f191c4ecfee
Cornelis <vanstratenc@gmail.com>
2018-05-30 22:56:16 +0000

    avatar upload functionality

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,31 @@ var passport        = require("passport");
 var User            = require("../models/user");
 
 
+//multer
+var multer          = require('multer');
+var storage         = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+//cloudinary
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'openclassroom', 
+  api_key: process.env.CLAPIKEY,
+  api_secret: process.env.CLAPISECRET
+});
+
 
 
 //ROOT route
@@ -78,19 +103,17 @@ router.get("/logout", function(req, res){
 
 //USER PROFILE PAGE SHOW ROUTE
 router.get("/users/:id", function(req, res){
-    //find the User matching the id in the DB
     User.findById(req.params.id).populate("circles").populate({path: "circles", populate: {path: "reviews"}}).populate({path: "joinedCircles", populate: {path: "endorsementRef"}}).exec(function(err, foundUser){
         if(err){
             console.log(err);
             req.flash("error", "This user doesn't exist");
             return res.redirect("back");
         } else {
-            //render the show template with that User and pass through found data
-            console.log(foundUser)
             res.render("users/show", {user: foundUser});
         }
     })
 })
+
 //EDIT FORM
 router.get("/users/:id/edit", middleware.checkUserProfileOwnership, function(req, res){
         User.findById(req.params.id, function(err, foundUser){
@@ -98,26 +121,24 @@ router.get("/users/:id/edit", middleware.checkUserProfileOwnership, function(req
             })
 })
 //UPDATE
-router.put("/users/:id/", middleware.checkUserProfileOwnership, function(req, res){
+router.put("/users/:id/", middleware.checkUserProfileOwnership, upload.single('avatar_upload'), function(req, res){
     var newData = req.body.user
+    if(req.file){console.log(req.file)}
+    if(req.files){console.log(req.files)}
       if(newData.description && newData.avatar && newData.firstName && newData.lastName){
                 newData.isEducator = true;
             } else {
                 newData.isEducator = false;
             }
-    console.log(newData);
-    //find and update User
-    User.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, updatedUser){
-        if(err){
-            console.log(err)
-            req.flash("error", err.message);
-            res.redirect("back")
-        } else {
-            req.flash("success", "User profile successfully updated")
-            res.redirect("/users/" + updatedUser.id)
-        }
-    })
-    //redirect to show page
+            if (req.file){
+                    cloudinary.uploader.destroy(req.body.oldAvatar, function(error, result){console.log("deleted:" + result)});
+                    cloudinary.uploader.upload(req.file.path, function(result) {
+                    newData.avatar = imageName(result.secure_url); 
+                    updateUser(newData, req, res);
+                    });
+                } else {
+                    updateUser(newData, req, res)
+                }
 })
 
 
@@ -190,6 +211,26 @@ var isFav = function(req){
     return answer;
 }
 
+var updateUser = function(newData, req, res){
+        User.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, updatedUser){
+        if(err){
+            console.log(err)
+            req.flash("error", err.message);
+            res.redirect("back")
+        } else {
+            req.flash("success", "User profile successfully updated")
+            res.redirect("/users/" + updatedUser.id)
+        }
+    })
+}
+
+
+var imageName = function(url){
+    var slashindex = url.lastIndexOf('/')+1
+    var dotindex = url.lastIndexOf('.') 
+    var file = url.slice(slashindex, dotindex)
+    return file
+}
 
 
 module.exports = router;
