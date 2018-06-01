f3c6ab7b58e7988fc31d17e5b29ceba2ddff6bf2
Gerrit Maatkamp <gerrit.maatkamp@gmail.com>
2018-06-01 06:29:36 +0000

    add pdp + sidebars

--- public/stylesheets/js/agency.js
+++ public/stylesheets/js/agency.js
@@ -76,3 +76,57 @@ var header = $(".cta-content");
           header.removeClass("cta-visible");
         }
 });
+
+$(window).scroll(function() {    
+  var scroll = $(window).scrollTop();
+    if (scroll >= 620) {
+        $('.pdp-mentor--name').addClass("rotate-out");
+      } else {
+        $('.pdp-mentor--name').removeClass("rotate-out");
+      }
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  
+$(window).on('scroll', function(){
+  if ($(this).scrollTop() > 800) {
+      $('.top').removeClass('is-hidden');
+  } else {
+      $('.top').addClass('is-hidden');
+  }
+});
